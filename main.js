196a0c737a2cc14fd10409bc66eae2bac685ff96
pandurisathwik <74193944+pandurisathwik@users.noreply.github.com>
2021-10-23 18:37:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status="";
+objects="";
 function preload() {
     img = loadImage("dog_cat.jpg")
 }
@@ -13,18 +14,21 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 640, 420)
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    rect(30, 60, 450, 350);
-    stroke("#FF0000");
+    
+    if(status != ""){
+for(i = 0; i< objects.length; i++
+){
+    document.getElementById("status").innerHTML = "Status : Objects Detected";
 
     fill("#FF0000");
-    text("Cat", 320, 120);
+    percent = floor(objects[i].confidence*100);
+    text(objects[i].label + " "+percent+"%", objects[i].x+15,objects[i].y+15);
     noFill();
     stroke("#FF0000");
-    rect(300, 90, 270, 320);
-
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+}
+   
+    }
 }
 
 function modelLoaded(){
@@ -38,4 +42,5 @@ if(error){
     console.error();
 }
 console.log(results);
+objects = results;
 }
