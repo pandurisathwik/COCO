913bdf2edb16c7f1adf81b5b682896ab06aa7710
pandurisathwik <74193944+pandurisathwik@users.noreply.github.com>
2021-10-25 18:37:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,50 @@
 img = "";
-status="";
-objects="";
+objects = [];
+status = "";
+
 function preload() {
-    img = loadImage("dog_cat.jpg")
+    img = loadImage('dog_cat.jpg');
 }
 
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
-    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
-function draw() {
-    image(img, 0, 0, 640, 420)
-    
-    if(status != ""){
-for(i = 0; i< objects.length; i++
-){
-    document.getElementById("status").innerHTML = "Status : Objects Detected";
-
-    fill("#FF0000");
-    percent = floor(objects[i].confidence*100);
-    text(objects[i].label + " "+percent+"%", objects[i].x+15,objects[i].y+15);
-    noFill();
-    stroke("#FF0000");
-    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
-}
-   
-    }
-}
-
-function modelLoaded(){
-    console.log("Model is Loaded...");
+function modelLoaded() {
+    console.log("Model Loaded!");
     status = true;
-    objectDetector.detect(img,gotResults);
 }
 
-function gotResults(error,results){
-if(error){
-    console.error();
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
-console.log(results);
-objects = results;
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
